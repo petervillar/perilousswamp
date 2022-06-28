aaab058551e30dc8080d09fdd43da1cdda43ef5c
Peter Villar <peter.villar@gmail.com>
2022-06-28 22:56:05 +0200

    begin movePlayer routine

--- perilousswamp.py
+++ perilousswamp.py
@@ -13,13 +13,6 @@ def clear():
         _ = system("clear")
 
 
-"""
-main()
-getPlayerMove()
-displayMap()
-"""
-
-
 def displayCredits():
     clear()
 
@@ -47,59 +40,102 @@ def displayCredits():
     input("To start press the enter key...")
     print("\n")
 
-def displayMap():
-    pass
 
 def getPlayerMove():
-    print('''Which way now? - N, S, E, W, OR SE, NW
-    etc, "MAP" or "OUT"''')
-    print('\n')
+    print(
+        '''Which way now? - N, S, E, W, OR SE, NW
+    etc, "MAP" or "OUT"'''
+    )
+    print("\n")
     playerMove = input(">>> ").upper().strip()
 
     return playerMove
 
+
 def initializeMap():
-    swampMap = [['#','#','#','#','#','#','#','#','#','#','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','.','.','.','.','.','.','.','.','.','#'],
-                ['#','#','#','#','#','#','#','#','#','#','#']]
+    swampMap = [
+        ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+        ["#", ".", ".", "X", " ", ".", ".", ".", ".", ".", "#"],
+        ["#", ".", ".", " ", ".", ".", ".", ".", ".", ".", "#"],
+        ["#", " ", ".", " ", ".", ".", ".", ".", ".", ".", "#"],
+        ["#", ".", ".", ".", ".", ".", ".", ".", " ", ".", "#"],
+        ["#", ".", ".", ".", ".", ".", ".", ".", ".", " ", "#"],
+        ["#", ".", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
+        ["#", ".", ".", ".", ".", ".", ".", ".", " ", " ", "#"],
+        ["#", ".", ".", ".", ".", ".", ".", ".", " ", ".", "#"],
+        ["#", ".", ".", ".", ".", ".", "*", " ", " ", ".", "#"],
+        ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
+    ]
 
-    gubbe = (1,1)
-    princess = (9,6)
+    gubbe = [1, 3]
+    princess = (9, 6)
 
-    swampMap[gubbe] = 'X'
+    swampMap[gubbe[0]][gubbe[1]] = "X"
+    swampMap[princess[0]][princess[1]] = "*"
 
     return swampMap, gubbe, princess
 
+
 def displayMap(swampMap):
+    print(" N")
+    print("W+E\tX = You, * = Princess, . = swamp, # = edge")
+    print(" S \t. = swamp, # = edge")
+    print()
+
     for row in swampMap:
-        print(*row, sep='')
+        print(*row, sep="")
+
+def movePlayer(gubbe, playerMove, swampMap):
+    if playerMove == "N":
+        northOfGubbe = gubbe[0] - 1
+        thingNorthOfGubbe = swampMap[northOfGubbe][gubbe[1]]
+        if thingNorthOfGubbe != "#":
+            gubbe[0] = northOfGubbe
+        elif thingNorthOfGubbe == "#":
+            endGame()
+
+def endGame():
+    print("You survived the swamp with % skill and % luck.")
+    print("You ripped off a total of % treasure points off the poor overworked monsters.")
+    print("% of them died protecting their rightful treasure.")
+    print("\nCongratulations!\n")
+    print("Pity about the princess...")
+    print("The wizard fed her to a dragon.")
+    print("The king is not at all that pleased.")
+    print("\nTry again?... You could get lucky.")
+
+    sys.exit()
 
 
 def main():
     """Runs a single game of Perilous Swamps"""
     displayCredits()
+    clear()
 
-    gubbe=()
-    princess=()
+    gubbe = ()
+    princess = ()
     swampMap, gubbe, princess = initializeMap()
 
+    displayMap(swampMap)
+    print()
+
     while True:
-        displayMap(swampMap)
-        print('\n')
         playerMove = getPlayerMove()
 
         if playerMove == "OUT":
             print("So long... better luck next time!")
             print("\n")
             sys.exit()
+
+        elif playerMove == "MAP":
+            clear()
+            displayMap(swampMap)
+
+        elif playerMove in ["N", "S", "E", "W", "NE", "NW", "SE", "SE"]:
+            movePlayer(gubbe, playerMove, swampMap)
+
+        else:
+            continue
 
 
 if __name__ == "__main__":
