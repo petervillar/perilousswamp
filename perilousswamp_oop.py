a13b319d718aab0201176ca10445a290de217a9a
Peter Villar <peter.villar@gmail.com>
2022-07-09 21:07:10 +0200

    handle encounters begin

--- perilousswamp_oop.py
+++ perilousswamp_oop.py
@@ -3,6 +3,7 @@ http://www.zx81stuff.org.uk/zx81/tape/FantasyGames"""
 
 import sys
 import time
+import random
 from os import system, name
 
 SWAMP = "·"
@@ -10,20 +11,21 @@ EDGE = "#"
 YOU = "X"
 PRINCE = "*"
 
+
 class Player:
     def __init__(self):
         self.skill = 0
+        self.combatStrength = random.randint(1500, 2000)
         self.luck = 0
         self.treasurePoints = 0
         self.killedMonsters = 0
-        self.playerPos = [1, 3]
 
 
 class PerilousSwampBoard:
-    def __init__(self, playerPos):
-        self.playerPos = playerPos
-
+    def __init__(self):
         # TODO: randomize
+        self.playerPos = [1, 3]
+        self.tmpPlayerPos = self.playerPos.copy()
         self.princePos = (6, 5)
         self.swampMap = [
             ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
@@ -36,18 +38,16 @@ class PerilousSwampBoard:
             ["#", "·", "·", " ", "·", " ", "·", "·", " ", "·", "#"],
             ["#", " ", " ", "·", " ", "·", " ", "·", "·", " ", "#"],
             ["#", " ", "·", "·", " ", " ", " ", "·", " ", "·", "#"],
-            ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"]
+            ["#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#"],
         ]
 
         self.swampMap[self.playerPos[0]][self.playerPos[1]] = YOU
         self.swampMap[self.princePos[0]][self.princePos[1]] = PRINCE
 
-    def updateMap(self, swampMap, oldPlayerPosition, playerPosition, princessPosition):
-        swampMap[oldPlayerPosition[0]][oldPlayerPosition[1]] = " "
-        swampMap[playerPosition[0]][playerPosition[1]] = "X"
-        swampMap[princessPosition[0]][princessPosition[1]] = "*"
-
-        return swampMap
+    def updateMap(self):
+        self.swampMap[self.oldPlayerPos[0]][self.oldPlayerPos[1]] = " "
+        self.swampMap[self.playerPos[0]][self.playerPos[1]] = "X"
+        self.swampMap[self.princePos[0]][self.princePos[1]] = "*"
 
     def displayMap(self):
         print(" N")
@@ -60,7 +60,6 @@ class PerilousSwampBoard:
 
     def movePlayer(self, playerMove):
         tmpPlayerPos = self.playerPos.copy()
-        print(tmpPlayerPos)
 
         if playerMove == "N":
             tmpPlayerPos[0] = tmpPlayerPos[0] - 1
@@ -83,7 +82,6 @@ class PerilousSwampBoard:
             tmpPlayerPos[0] = tmpPlayerPos[0] + 1
             tmpPlayerPos[1] = tmpPlayerPos[1] - 1
 
-        print(tmpPlayerPos)
         thingPlayerPos = self.swampMap[tmpPlayerPos[0]][tmpPlayerPos[1]]
 
         if thingPlayerPos == EDGE:
@@ -91,13 +89,20 @@ class PerilousSwampBoard:
         elif thingPlayerPos == SWAMP:
             print("Too wet that way, clot.\n")
         else:
+            self.oldPlayerPos = self.playerPos.copy()
             self.playerPos = tmpPlayerPos.copy()
+
+
+class SwampEncounter:
+
+
 
 def clearScreen():
     if name == "nt":
         _ = system("cls")
     else:
         _ = system("clear")
+
 
 def displayCredits():
 
@@ -125,7 +130,8 @@ def displayCredits():
     input("To start press the enter key...")
     print("\n")
 
-def endGame():
+
+def endGame(p):
     print("You survived the swamp with % skill and % luck.")
     print(
         "You ripped off a total of % treasure points off the poor overworked monsters."
@@ -139,6 +145,7 @@ def endGame():
 
     sys.exit()
 
+
 def getPlayerMove():
     print(
         '''Which way now? - N, S, E, W, OR SE, NW
@@ -149,14 +156,17 @@ def getPlayerMove():
 
     return playerMove
 
+
 def main():
     """Runs a single game of Perilous Swamps"""
+    player = Player()
+    swampMap = PerilousSwampBoard()
+    swampEncounter = SwampEncounter()
+
     clearScreen()
     displayCredits()
     clearScreen()
 
-    player = Player()
-    swampMap = PerilousSwampBoard(player.playerPos)
     swampMap.displayMap()
 
     while True:
@@ -172,12 +182,9 @@ def main():
             swampMap.displayMap()
 
         elif playerMove in ["N", "S", "E", "W", "NE", "NW", "SE", "SW"]:
-#            oldPlayerPos = playerPos.copy()
-#            playerPosition = swampMap.movePlayer(playerPosition, playerMove)
-#            swampMap = updateMap(
-#                swampMap, oldPlayerPosition, playerPosition, princessPosition
-#            )
             swampMap.movePlayer(playerMove)
+            swampMap.updateMap()
+            swampEncounter(swampMap, player)
 
         else:
             continue
